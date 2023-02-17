775008842f598d0cefa648c916cd0cd0d8fad771
GeraltShaowen233 <110619351+GeraltShaowen233@users.noreply.github.com>
2023-02-17 15:16:02 +0000

    delete build

--- src/core/Render/GraphicsContext.h
+++ src/core/Render/GraphicsContext.h
@@ -5,10 +5,20 @@
 #ifndef SPARROW_GRAPHICSCONTEXT_H
 #define SPARROW_GRAPHICSCONTEXT_H
 
+#include <memory>
+namespace SPW
+{
+    class GraphicsContext
+    {
+    public:
+        virtual void Init() = 0;
+        virtual void SwapBuffers() = 0;
 
-class GraphicsContext {
+        static std::shared_ptr<GraphicsContext> Create(void* window);
 
-};
+    };
+}
+
 
 
 #endif //SPARROW_GRAPHICSCONTEXT_H
--- src/core/Render/RenderAPII.h
+++ src/core/Render/RenderAPII.h
@@ -4,5 +4,19 @@
 
 #ifndef SPARROW_RENDERAPII_H
 #define SPARROW_RENDERAPII_H
+namespace SPW
+{
+    class RenderAPII
+    {
+    public:
+        enum class RenderAPIType
+        {
+            None = 0,
+            OpenGL,
+        };
+
+    };
+
+}
 
 #endif //SPARROW_RENDERAPII_H
