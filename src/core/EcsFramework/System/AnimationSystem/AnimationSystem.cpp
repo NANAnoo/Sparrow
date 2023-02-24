e84e58b86a61c276303e603a8df1d609802d5a15
Shawwy <yao505822174@gmail.com>
2023-02-24 21:18:57 +0000

    info : Adding new classes and header files. Adding new CMakeLists files to those modules. opt: Adding new compile target to original CMakeLists files

---
+++ src/core/EcsFramework/System/AnimationSystem/AnimationSystem.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Shawwy on 2/24/2023.
+//
+
+#include "AnimationSystem.h"
---
+++ src/core/EcsFramework/System/AnimationSystem/AnimationSystem.h
@@ -0,0 +1,14 @@
+//
+// Created by Shawwy on 2/24/2023.
+//
+
+#ifndef SPARROW_ANIMATIONSYSTEM_H
+#define SPARROW_ANIMATIONSYSTEM_H
+
+
+class AnimationSystem{
+
+};
+
+
+#endif //SPARROW_ANIMATIONSYSTEM_H
