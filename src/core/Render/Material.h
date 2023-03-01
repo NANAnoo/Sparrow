0565a9f43edc7bb153f6c4411b1748b514f01521
1072081744@qq.com <1072081744@qq.com>
2023-03-01 21:23:41 +0000

    [fe] material fe

---
+++ src/core/Render/Material.h
@@ -0,0 +1,41 @@
+//
+// Created by sgsdi on 2023/2/22.
+//
+
+#ifndef SPARROW_MATERIAL_H
+#define SPARROW_MATERIAL_H
+
+#include <string>
+#include <map>
+namespace SPW
+{
+    enum class TextureType
+    {
+        Albedo = 0,
+        Normal,
+        Metalness,
+        Roughness,
+        AmbientOcclusion,
+    };
+
+
+    class Material
+    {
+    public:
+        std::map<TextureType, std::string> TextureMap;
+
+        void updateTexture(TextureType type,std::string texturePath)
+        {
+            if(texturePath=="")
+            {
+                TextureMap.erase(type);
+                return;
+            }
+
+            TextureMap[type] = texturePath;
+        }
+
+    };
+}
+
+#endif //SPARROW_MATERIAL_H
